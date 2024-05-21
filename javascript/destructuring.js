0e3e6ce48849d7cee5600dd69cf3a59f5edd9def
shogo_koide <shogo10261026@gmail.com>
2024-05-21 09:20:00 +0900

    add: 分割代入

---
+++ javascript/destructuring.js
@@ -0,0 +1,17 @@
+// 分割代入（ドット記法で記載していくと長くなってしまうので、この書き方ができた）
+//
+const lang = {ja: "日本語", en: "英語", fr: "フランス語", de: {h: "ドイツ語", i: "ほげ語"}};
+const {ja, en, fr, de} = lang;
+console.log(ja); // 日本語
+console.log(en); // 英語
+console.log(fr); // フランス語
+console.log(de.h); // ドイツ語
+
+// オブジェクトは全てミュータブルである
+const key = 'key-string';
+const obj = {
+    [key]: 'value-string'
+}
+console.log(obj[key]); // value-string
+
+
